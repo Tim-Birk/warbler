5f63f8dfaa6837cdf944b3e73d7512816cd775a5
Tim Birkmire <timbirkmire@yahoo.com>
2021-02-04 21:55:52 -0500

    Implement logout route

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -45,12 +45,15 @@ def do_login(user):
 
     session[CURR_USER_KEY] = user.id
 
-
+@app.route('/logout')
 def do_logout():
     """Logout user."""
 
     if CURR_USER_KEY in session:
         del session[CURR_USER_KEY]
+        flash(f"You have been logged out.", "success")
+
+    return redirect('/login')
 
 
 @app.route('/signup', methods=["GET", "POST"])
